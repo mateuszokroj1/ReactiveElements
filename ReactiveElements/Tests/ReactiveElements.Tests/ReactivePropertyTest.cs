6aa8a6956ce1d95af6d43dd583bfddfd3a56239d
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-08-13 09:12:49 +0200

    Tests project created

---
+++ ReactiveElements/Tests/ReactiveElements.Tests/ReactivePropertyTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace ReactiveElements.Tests
+{
+    public class ReactivePropertyTest
+    {
+        #region Constructor
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        #endregion
+    }
+}
