98ab9a9844263166154ee2a7d91b70daa7d5fd79
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-08-26 23:07:23 +0200

    ReadonlyReactiveProperty test created.

---
+++ ReactiveElements/Tests/ReactiveElements.Tests/ReadonlyReactivePropertyTest.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveElements;
+
+using Xunit;
+
+namespace ReactiveElements.Tests
+{
+    public class ReadonlyReactivePropertyTest
+    {
+        #region Constructors
+
+        [Fact]
+        public void Constructor1_ShouldNotThrowAndSetValueToDefault()
+        {
+
+        }
+
+        #endregion
+
+        #region Property_Value
+
+        [Fact]
+        public void Value_Get_ShouldReturnActualValue()
+        {
+
+        }
+
+        #endregion
+    }
+}
