0da8086693f0d14195487fa0ddf503bbc126a2c8
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-09-06 01:06:55 +0200

    Created and implemented TestModel for ReactiveProperty. Created tests for ReactiveCommand.

---
+++ ReactiveElements/Tests/ReactiveElements.Tests/ReactiveCommandTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using Xunit;
+
+namespace ReactiveElements.Tests
+{
+    public class ReactiveCommandTest
+    {
+        #region Constructor
+
+        [Fact]
+        public void Constructor_WhenArgumentIsNull_ShouldThrowArgumentNullException()
+        {
+            object argument;
+            Assert.Throws<ArgumentNullException>(() => new ReactiveCommand(arg => argument = arg, null));
+            Action<object> action1 = null;
+            Action action2 = null;
+            Assert.Throws<ArgumentNullException>(() => new ReactiveCommand(action1, new ReadonlyReactiveProperty<bool>(true)));
+            Assert.Throws<ArgumentNullException>(() => new ReactiveCommand(action2, new ReadonlyReactiveProperty<bool>(true)));
+        }
+
+        [Fact]
+        public void Constructor_WhenArgumentsIsValid_ShouldInitializeProperly()
+        {
+
+        }
+
+        #endregion
+    }
+}
