e445e628d816cbe9813b26f2455b8743147e36cb
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-09-06 22:20:44 +0200

    New tests for ReactiveCommand

--- ReactiveElements/Tests/ReactiveElements.Tests/ReactiveCommandTest.cs
+++ ReactiveElements/Tests/ReactiveElements.Tests/ReactiveCommandTest.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Reactive.Linq;
+using System.Reflection.Metadata;
 
 using Xunit;
 
@@ -21,7 +21,99 @@ namespace ReactiveElements.Tests
         }
 
         [Fact]
-        public void Constructor_WhenArgumentsIsValid_ShouldInitializeProperly()
+        public void Constructor1_WhenArgumentsIsValid_ShouldBeInitialized()
+        {
+            bool executed = false;
+            bool eventTriggered = false;
+            var canExecuteProperty = new ReactiveProperty<bool>(false);
+            var command = new ReactiveCommand(() => executed = true, canExecuteProperty);
+            command.CanExecuteChanged += (sender, e) => eventTriggered = true;
+            
+            Assert.False(command.CanExecute(null));
+
+            canExecuteProperty.Value = true;
+            Assert.True(command.CanExecute(null));
+            Assert.True(eventTriggered);
+
+            command.Execute(null);
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void Constructor2_WhenArgumentsIsValid_ShouldBeInitialized()
+        {
+            bool executed = false;
+            bool eventTriggered = false;
+            var canExecuteProperty = new ReactiveProperty<bool>(false);
+            var command = new ReactiveCommand(parameter => executed = true, canExecuteProperty);
+            command.CanExecuteChanged += (sender, e) => eventTriggered = true;
+
+            Assert.False(command.CanExecute(null));
+
+            canExecuteProperty.Value = true;
+            Assert.True(command.CanExecute(null));
+            Assert.True(eventTriggered);
+
+            command.Execute(null);
+            Assert.True(executed);
+        }
+
+        #endregion
+
+        #region Methods
+
+        [Fact]
+        public void CanExecute_ShouldReturnBoolWithoutParameter()
+        {
+            bool executed = false;
+            var command = new ReactiveCommand(() => executed = true, new ReactiveProperty<bool>(true));
+            Assert.True(command.CanExecute(null));
+            Assert.True(command.CanExecute(324));
+
+            command = new ReactiveCommand(parameter => executed = true, new ReactiveProperty<bool>(false));
+            Assert.False(command.CanExecute(null));
+            Assert.False(command.CanExecute(""));
+        }
+
+        [Fact]
+        public void Execute_ShouldRun()
+        {
+            bool executed = false;
+            var command = new ReactiveCommand(() => executed = true, new ReactiveProperty<bool>(true));
+            command.Execute(null);
+            Assert.True(executed);
+
+            executed = false;
+            command = new ReactiveCommand(parameter => executed = true, new ReactiveProperty<bool>(false));
+            command.Execute(null);
+            command.CanExecute("");
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void OnNext_ShouldChangeCanExecuteValue()
+        {
+            bool executed = false;
+            var command = new ReactiveCommand(() => executed = true, new ReactiveProperty<bool>(true));
+            Assert.True(command.CanExecute(null));
+
+            command.OnNext(false);
+            Assert.False(command.CanExecute(""));
+
+            command.OnNext(true);
+            Assert.True(command.CanExecute(12));
+        }
+
+        [Fact]
+        public void OnCompleted_ShouldNotThrow()
+        {
+            bool executed = false;
+            var command = new ReactiveCommand(() => executed = true, new ReactiveProperty<bool>(true));
+            command.OnCompleted();
+        }
+
+        [Fact]
+        public void OnError_ShouldSetLastError()
         {
 
         }
