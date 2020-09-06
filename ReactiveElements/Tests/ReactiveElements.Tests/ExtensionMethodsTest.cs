e049e188238771f46b2ca104004fe0b2b4704da4
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-09-07 00:36:12 +0200

    Created tests for ExtensionMethods.

---
+++ ReactiveElements/Tests/ReactiveElements.Tests/ExtensionMethodsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace ReactiveElements.Tests
+{
+    public class ExtensionMethodsTest
+    {
+        #region GetReactiveProperty
+
+        [Fact]
+        public void GetReactiveProperty_WhenArgumentIsNull_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ExtensionMethods.GetReactiveProperty<TestModel, string>(null, model => model.Property1));
+            Assert.Throws<ArgumentNullException>(() => ExtensionMethods.GetReactiveProperty<TestModel, string>(new TestModel(), null));
+        }
+
+        [Fact]
+        public void GetReactiveProperty_WhenArgumentsIsValid_ShouldReturnReactiveProperty()
+        {
+            var model = new TestModel();
+            var property = model.GetReactiveProperty(m => m.Property1);
+            Assert.IsType<ReactiveProperty<string>>(property);
+            Assert.Equal(model.Property1, property.Value);
+        }
+
+        #endregion
+    }
+}
