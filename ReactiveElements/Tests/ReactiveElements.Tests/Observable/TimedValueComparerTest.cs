596a31f800afa997c8a48f7227b9591a640b4c18
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-09-13 01:04:36 +0200

    Created tests for TimedValueComparer and Unsubscriber.

---
+++ ReactiveElements/Tests/ReactiveElements.Tests/Observable/TimedValueComparerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace ReactiveElements.Tests.Observable
+{
+    public class TimedValueComparerTest
+    {
+        #region Constructors
+
+
+
+        #endregion
+
+        #region Properties
+
+        [Fact]
+        public void SynchronizationContext_GetSet_ShouldSetAndReturnValue()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ ReactiveElements/Tests/ReactiveElements.Tests/Observable/UnsubscriberTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+using ReactiveElements.Observable;
+
+namespace ReactiveElements.Tests.Observable
+{
+    public class UnsubscriberTest
+    {
+        #region Constructor
+
+        [Fact]
+        public void Constructor_WhenArgumentIsNull_ShouldThrowArgumentNullException()
+        {
+            //Assert.Throws<ArgumentNullException>(() => new Unsubscriber<bool>());
+        }
+
+        #endregion
+    }
+}
