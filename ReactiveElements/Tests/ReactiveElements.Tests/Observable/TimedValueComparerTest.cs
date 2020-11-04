9067a2ae8638603c19ce71cc0cadd3f2157fb28e
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-11-04 18:19:42 +0100

    Customization of TimedValueComparerTest class.

--- ReactiveElements/Tests/ReactiveElements.Tests/Observable/TimedValueComparerTest.cs
+++ ReactiveElements/Tests/ReactiveElements.Tests/Observable/TimedValueComparerTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+
+using ReactiveElements.Observable;
 
 using Xunit;
 
@@ -10,17 +13,26 @@ namespace ReactiveElements.Tests.Observable
     {
         #region Constructors
 
-
+        [Fact]
+        public void Constructor_ShouldInitializeAndSetValue()
+        {
+            var timedValueComparer = new TimedValueComparer<double>(() => Math.PI, TimeSpan.FromMilliseconds(10));
+            Assert.Equal(Math.PI, timedValueComparer.CurrentValue);
+        }
 
         #endregion
 
         #region Properties
 
         [Fact]
-        public void SynchronizationContext_GetSet_ShouldSetAndReturnValue()
+        public void SynchronizationContext_Get_ShouldReturnDefaultValue()
         {
-
+            var timedValueComparer = new TimedValueComparer<bool>(() => true, TimeSpan.FromSeconds(10));
+            Assert.Equal(SynchronizationContext.Current, timedValueComparer.SynchronizationContext);
+            timedValueComparer.Dispose();
         }
+
+        
 
         #endregion
     }
